17b6d6043cc997bafcb356b8b6508398df57ca8d
Leo Rimmer <leo@petdirect.co.nz>
2025-06-13 15:52:47 +1200

    error handling

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -4,34 +4,50 @@ import Button from 'react-bootstrap/Button';
 import Stack from 'react-bootstrap/Stack';
 import Toast from 'react-bootstrap/Toast';
 import ToastContainer from 'react-bootstrap/ToastContainer';
-import { RiFileCopyLine, RiCheckLine } from '@remixicon/react'
+import {
+    RiFileCopyLine,
+    RiCheckboxCircleLine,
+    RiErrorWarningLine
+} from '@remixicon/react'
 
 enum ButtonStates {
     initial = 'Copy email',
-    copied = 'Copied'
+    copied = 'Copied',
+    failed = 'Failed to copy'
+}
+
+interface ToastProps {
+    variant: 'success'|'danger',
+    message: string
 }
 
 const CopyButton: React.FunctionComponent<Button.ButtonProps> = ({ ...props }) => {
     const [copiedText, copy] = useCopyToClipboard();
     const [buttonState, setButtonState] = useState<ButtonStates>(ButtonStates.initial);
-    const [showToast, setShowToast] = useState<boolean>(false);
+    const [toast, setToast] = useState<ToastProps|null>(null);
 
     const handleCopy = () => {
         copy('leo@rimmer.co')
             .then(() => {
                 setButtonState(ButtonStates.copied);
-                setShowToast(true);
+                setToast({
+                    variant: 'success',
+                    message: "Email address copied"
+                });
             })
             .catch(error => {
-                console.error('Failed to copy!', error)
+                setButtonState(ButtonStates.failed);
+                setToast({
+                    variant: 'danger',
+                    message: "Failed to copy email address"
+                });
             })
     }
 
     useEffect(() => {
-        if (buttonState === ButtonStates.copied) {
+        if (buttonState !== ButtonStates.initial) {
             const timer = setTimeout(() => {
-                setButtonState(ButtonStates.initial);
-                setShowToast(false);
+                setButtonState(ButtonStates.initial)
             }, 2000);
             
             return () => clearTimeout(timer);
@@ -48,7 +64,7 @@ const CopyButton: React.FunctionComponent<Button.ButtonProps> = ({ ...props }) =
             >
                 <Stack direction="horizontal" gap={2}>
                     {buttonState === ButtonStates.copied ? (
-                        <RiCheckLine />
+                        <RiCheckboxCircleLine />
                     ) : (
                         <RiFileCopyLine />
                     )}
@@ -60,11 +76,18 @@ const CopyButton: React.FunctionComponent<Button.ButtonProps> = ({ ...props }) =
                 position="top-end"
                 className="p-3"
             >
-                <Toast show={showToast}>
+                <Toast
+                    show={buttonState !== ButtonStates.initial} 
+                    onExited={() => setToast(null)}
+                >
                     <Toast.Body>
                         <Stack direction="horizontal" gap={2}>
-                            <RiCheckLine className="text-success" />
-                            <span>Email address copied</span>
+                            {toast?.variant === 'success' ? (
+                                <RiCheckboxCircleLine className="text-success" />
+                            ) : (
+                                <RiErrorWarningLine className="text-danger" />
+                            )}
+                            <span>{toast?.message}</span>
                         </Stack>
                     </Toast.Body>
                 </Toast>
